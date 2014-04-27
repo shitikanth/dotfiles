e6f9de95a9d51f2b2a6fbef29311c324f1d78bc3
Shitikanth <shitikanth1@gmail.com>
2014-04-27 13:40:18 -0400

    .bin/weekly: removed bug
    Former-commit-id: 24aec42f61058fee4234291ef7734af85a04bd13

--- .bin/weekly.sh
+++ .bin/weekly.sh
@@ -4,7 +4,7 @@ git pull origin master
 git commit -am "weekly commit"
 git push origin master
 
-if [ ! -d "$HOME/Dropbox" ]; then
+if [ -d "$HOME/Dropbox" ]; then
     rsync -avz $HOME/Documents/work/research $HOME/Dropbox
 fi
 
