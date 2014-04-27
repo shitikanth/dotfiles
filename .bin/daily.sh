6156ede3a857949879ab5ef053d88c1f373b9fb3
Shitikanth Kashyap <golu3990@gmail.com>
2014-04-27 13:29:43 -0400

    weekly commit
    Former-commit-id: 44fea59165467c5f91d80a8b6b007822745db65e

---
+++ .bin/daily.sh
@@ -0,0 +1,1 @@
+$HOME/.org/sync.sh
---
+++ .bin/weekly.sh
@@ -0,0 +1,10 @@
+$HOME/.bin/daily.sh
+
+git pull origin master
+git commit -am "weekly commit"
+git push origin master
+
+if [ ! -d "$HOME/Dropbox" ]; then
+    rsync -avz $HOME/Documents/work/research $HOME/Dropbox
+fi
+
