99d8fc9d3615dc29d31c31fb4ec75b59ea02aca1
Shitikanth Kashyap <shitikanth1@gmail.com>
2014-05-10 10:14:02 -0400

    weekly commit

--- .emacs.d/elpa/org-plus-contrib-20140203/org-plus-contrib-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "org-plus-contrib"
-  "20140203" "Outline-based notes management and organizer" ( ))
-;; no-byte-compile: t
--- .emacs.d/site-lisp/basic-settings.el
+++ .emacs.d/site-lisp/basic-settings.el
@@ -1,26 +1,25 @@
 ;;; basic-settings.el --- My basic customizations and settings
 
-;; Copyright (C) 2014  Shitikanth Kashyap
+;;
+;; Start server
+(require 'server)
+(when (and (functionp 'server-running-p) (not (server-running-p)))
+  (server-start))
 
-;; Author: Shitikanth Kashyap <shitikanth1@gmail.com>
-;; Keywords: 
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-;;; Code:
+;;
+;; File behaviour
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+; (setq make-backup-files nil)	; disable backups
+(auto-compression-mode t)
 
 
+;;
+;; Gui settings
+(tool-bar-mode 0)
+(setq inhibit-startup-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t
+      fill-column-mode 81)
 
 ;;
 ;; Use package manager 
@@ -30,10 +29,38 @@
     '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives 
     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives 
+    '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
 
+;;
+;; Editing behaviour
+(auto-insert-mode t)			; provide templates for new files
 
-(setq make-backup-files nil) ; stop creating ~ files
+
+;;
+;; Options
+; (iswitchb-mode t)			; fast way to switch buffers
+(ido-mode t)				; even faster
+(setq diary-file "~/.emacs.d/diary")
+
+;;
+;; Enable disabled commands
+
+(put 'downcase-region  'disabled nil)   ; Let downcasing work
+(put 'erase-buffer     'disabled nil)
+(put 'eval-expression  'disabled nil)   ; Let ESC-ESC work
+(put 'narrow-to-page   'disabled nil)   ; Let narrowing work
+(put 'narrow-to-region 'disabled nil)   ; Let narrowing work
+(put 'set-goal-column  'disabled nil)
+(put 'upcase-region    'disabled nil)   ; Let upcasing work
+
+;;
+;; Personal information
+(setq user-full-name "Shitikanth Kashyap"
+      user-mail-address "shitikanth1@gmail.com")
 
 (provide 'basic-settings)
 ;;; basic-settings.el ends here
+
+
