500cb582454a94b6e8fc77e375eb8c9a3a970513
Shitikanth Kashyap <shitikanth1@gmail.com>
2016-11-24 16:05:09 -0500

    Changed naming convention.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,9 @@ git pull origin master
 DIR=`pwd`
 
 function create_symlinks() {
-    for file_name in `ls -A | egrep -v '.git*|bootstrap.sh|tmp'`; do ln -s $DIR/$file_name ~; done
+  for file_name in $(find . -maxdepth 1 -name '_*' -printf '%f\n')
+  do ln -sf $PWD/$file_name $HOME/$(echo $file_name | sed s/_/./1)
+  done
 }
 
 git submodule update --init --recursive
