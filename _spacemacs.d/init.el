aeb973c4cbef10607df7a5179032db6a2aae84f9
Shitikanth Kashyap <shitikanth1@gmail.com>
2017-08-29 14:14:41 -0400

    Add rainbow-mode to spacemacs.

--- _spacemacs.d/init.el
+++ _spacemacs.d/init.el
@@ -59,7 +59,7 @@ This function should only modify configuration layer settings."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(w3m)
+   dotspacemacs-additional-packages '(w3m rainbow-mode)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
