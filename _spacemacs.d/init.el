c537c20874093a8d58daaf29249fef2cf68e990b
Shitikanth Kashyap <shitikanth1@gmail.com>
2017-11-30 10:35:04 -0500

    [emacs] install try mode automatically

--- _spacemacs.d/init.el
+++ _spacemacs.d/init.el
@@ -63,7 +63,7 @@ This function should only modify configuration layer settings."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(rainbow-mode)
+   dotspacemacs-additional-packages '(rainbow-mode try)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
