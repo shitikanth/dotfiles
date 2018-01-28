f8f713a4279a23a839713cde092febce507256da
Shitikanth Kashyap <shitikanth1@gmail.com>
2018-01-28 17:16:00 -0500

    [zsh] Add some zsh-only aliases

---
+++ _aliases.zsh
@@ -0,0 +1,15 @@
+# Inspired from zsh-lovers
+
+alias -g C='| wc -l'
+alias -g H='| head'
+alias -g T='| tail'
+alias -g S='| sort'
+alias -g L='| less'
+alias -g LL='2>&1 | less'
+alias -g NUL="> /dev/null 2>&1"
+alias -g NE="2> /dev/null"
+alias -g G='| egrep'
+alias -g X='| xargs'
+
+# My own
+alias -g U='--user'
