b1fb9305284e419f4561fd1ea18d56036f94debb
Shitikanth Kashyap <shitikanth1@gmail.com>
2018-08-30 13:02:24 +0530

    [emacs.old] bootstrap use-package

--- _emacs.d.old/site-lisp/plugin-settings.el
+++ _emacs.d.old/site-lisp/plugin-settings.el
@@ -1,11 +1,19 @@
 (provide 'plugin-settings)
 
 ;;
-;; Use package manager
+;; Bootstrap use-package
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
 
 ;; List of packages I want to install automatically
 (setq package-list
